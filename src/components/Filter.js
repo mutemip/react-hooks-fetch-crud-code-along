4bfba6769e56fcb3e2fc30a404ea9038decbb61f
mutemip <paulmutemi2030@gmail.com>
2022-07-22 02:28:08 +0300

    React Fetch CRUD Codealong

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,8 +6,7 @@ function Filter({ category, onCategoryChange }) {
       <select
         name="filter"
         value={category}
-        onChange={(e) => onCategoryChange(e.target.value)}
-      >
+        onChange={(e) => onCategoryChange(e.target.value)}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
